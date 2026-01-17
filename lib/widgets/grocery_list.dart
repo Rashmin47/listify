e9ffbbf406929c215d52a5a329c8bddd61023bf9
rashmin <rashminxperience@gmail.com>
2026-01-17 14:25:01 +0545

    Added backend functionality to delete data

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,8 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -29,15 +31,29 @@ class _GroceryListState extends State<GroceryList> {
       'listify.json',
     );
     final response = await http.get(url);
+
+    if(response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+
+    if(response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later';
+      });
+
+
+    }
     final Map<String, dynamic> listData = json.decode(
       response.body,
     );
-    final List<GroceryItem> _loadedItems = [];
+    final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
       final category = categories.entries.firstWhere(
         (catItem) => catItem.value.title == item.value['category'],
       ).value;
-      _loadedItems.add(
+      loadedItems.add(
         GroceryItem(
           id: item.key,
           name: item.value['name'],
@@ -47,27 +63,53 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
     setState(() {
-      _groceryItems = _loadedItems;
+      _groceryItems = loadedItems;
+      _isLoading = false;
     });
 
   }
 
   void _addItem() async {
-    await Navigator.of(
+    final newItem = await Navigator.of(
       context,
     ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => const NewItem()));
-    _loadItems();
+if(newItem == null){
+  return;
+}
+
+setState(() {
+  _groceryItems.add(newItem);
+});
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https(
+      'a2zecom-default-rtdb.firebaseio.com',
+      'listify/${item.id}.json',
+    );
+    final response =  await http.delete(url);
+
+    if(response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+
   }
 
   @override
   Widget build(BuildContext context) {
+
     Widget content = Center(child: const Text('No items added yet'));
+    if(_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
         itemCount: _groceryItems.length,
@@ -87,6 +129,10 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+
+    if(_error != null) {
+      content =  Center(child: Text(_error!),);
     }
     return Scaffold(
       appBar: AppBar(
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -19,9 +19,13 @@ class _NewItemState extends State<NewItem> {
   var _enteredName;
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isSending = true;
+      });
       _formKey.currentState!.save();
       final response = await http.post(
         url,
@@ -32,11 +36,14 @@ class _NewItemState extends State<NewItem> {
           'category': _selectedCategory.title,
         }),
       );
-
-      if(!context.mounted){
-       return;
+      final Map<String, dynamic> resData = json.decode(response.body);
+      if (!context.mounted) {
+        return;
       }
-      Navigator.of(context).pop();
+      Navigator.of(context).pop(GroceryItem(id: resData['name'],
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory));
     }
   }
 
@@ -56,8 +63,12 @@ class _NewItemState extends State<NewItem> {
                 validator: (value) {
                   if (value == null ||
                       value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
+                      value
+                          .trim()
+                          .length <= 1 ||
+                      value
+                          .trim()
+                          .length > 50) {
                     return 'Must be 1 and 50 characters';
                   }
                   return null;
@@ -122,12 +133,16 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                    onPressed: () {
+                    onPressed: _isSending ?  null : () {
                       _formKey.currentState!.reset();
                     },
                     child: const Text('Reset'),
                   ),
-                  ElevatedButton(onPressed: _saveItem, child: Text('Add Item')),
+                  ElevatedButton(onPressed: _isSending ?  null : _saveItem, child: _isSending ? const SizedBox(
+                    height: 16,
+                    width: 16,
+                    child: CircularProgressIndicator(),
+                  ): Text('Add Item')),
                 ],
               ),
             ],
